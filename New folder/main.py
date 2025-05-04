ac45ebd077ea51b0082304fd2d0796be40fe7ef4
19anujshukla <143541232+19anujshukla@users.noreply.github.com>
2025-05-04 19:46:56 +0530

    Delete New folder/main.py

--- New folder/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-import pandas as pd
-
-app = FastAPI()
-
-@app.get("/assessments")
-def get_assessments():
-    df = pd.read_pickle("assessment_data.pkl")
-    return df.to_dict(orient="records")
